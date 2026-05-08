06a7916040f8e42bab14a388bd45c85b496bd57e
Senavarrete2775 <se.navarretep@duocuc.cl>
2026-05-07 23:48:33 -0400

    feat: setup inicial del microservicio de proyectos siguiendo arquetipo maven

---
+++ src/main/java/cl/duoc/fs3/proyectos/fs3_ms_proyectos/Fs3MsProyectosApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.fs3.proyectos.fs3_ms_proyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fs3MsProyectosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fs3MsProyectosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cl/duoc/fs3/proyectos/fs3_ms_proyectos/Fs3MsProyectosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.fs3.proyectos.fs3_ms_proyectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fs3MsProyectosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
